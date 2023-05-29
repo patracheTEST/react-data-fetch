f218a84833b4bdf58490242fd82b7deb836e8e92
patrache <ssk5442@naver.com>
2023-05-29 15:59:02 +0900

    [feat]: msw initialized

---
+++ src/mocks/handlers.js
@@ -0,0 +1,20 @@
+import { rest } from "msw";
+
+export const handlers = [
+    
+    rest.get('localhost:8888/test/', (req, res, ctx) => {
+
+    }),
+
+    rest.get('localhost:8888/test/', (req, res, ctx) => {
+        
+    }),
+
+    rest.get('localhost:8888/test/', (req, res, ctx) => {
+        
+    }),
+
+    rest.get('localhost:8888/test/', (req, res, ctx) => {
+        
+    })
+]
